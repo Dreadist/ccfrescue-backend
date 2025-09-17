30840375213a139b177bfd6b0e843d555baa1f75
Dreadist <slademaster@gmail.com>
2025-09-16 19:46:37 -0500

    Fix CORS for production domain

--- index.js
+++ index.js
@@ -209,7 +209,9 @@ const createContactEmailTemplate = (formData) => {
 // Middleware
 app.use(cors({
   origin: ['http://localhost:5173', 'http://localhost:5174', 'https://yourdomain.com', 'https://ccfrescue.org', 'https://www.ccfrescue.org'],
-  credentials: true
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express.json());
 
